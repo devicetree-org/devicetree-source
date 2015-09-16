20917f302cf94ea8d004b43bbe08637f79a9caec
Ian Campbell <ian.campbell@citrix.com>
2015-09-16 17:05:16 +0100

    index-filter.sh: Rewrap a bit for clarity.

--- scripts/index-filter.sh
+++ scripts/index-filter.sh
@@ -3,7 +3,8 @@
 set -e
 set -o pipefail
 
-git ls-files -s | tee /tmp/bar | sed -n -f ${SCRIPTS}/rewrite-paths.sed | tee /tmp/foo | \
+git ls-files -s | tee /tmp/bar | \
+	sed -n -f ${SCRIPTS}/rewrite-paths.sed | tee /tmp/foo | \
 	GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
 
 if [ -f "$GIT_INDEX_FILE.new" ] ; then
