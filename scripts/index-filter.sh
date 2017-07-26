cea98c844ab4249cac79566e2d7f009b945f861e
Ian Campbell <ijc@hellion.org.uk>
2017-07-26 08:59:55 +0100

    Remove crumbs trail
    I don't remember ever consulting it.

--- scripts/index-filter.sh
+++ scripts/index-filter.sh
@@ -3,10 +3,9 @@
 set -e
 set -o pipefail
 
-crumbs=/tmp/device-tree-export
-git ls-files -s | tee $crumbs-orig | \
-	${SCRIPTS}/flatten-symlinks.sh | tee $crumbs-flattened | \
-	sed -n -f ${SCRIPTS}/rewrite-paths.sed | tee $crumbs-rewritten | \
+git ls-files -s | \
+	${SCRIPTS}/flatten-symlinks.sh | \
+	sed -n -f ${SCRIPTS}/rewrite-paths.sed | \
 	GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
 
 if [ -f "$GIT_INDEX_FILE.new" ] ; then
