54e200d49e0ffc8d66429584d24aee7a185e2d34
Ian Campbell <ijc@hellion.org.uk>
2017-08-01 20:47:13 +0100

    Rename flatten-symlinks.pl to rewrite-index.pl

--- scripts/index-filter.sh
+++ scripts/index-filter.sh
@@ -3,8 +3,7 @@
 set -e
 set -o pipefail
 
-${SCRIPTS}/flatten-symlinks.pl | \
-	GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
+${SCRIPTS}/rewrite-index.pl | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
 
 if [ -f "$GIT_INDEX_FILE.new" ] ; then
     mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"
