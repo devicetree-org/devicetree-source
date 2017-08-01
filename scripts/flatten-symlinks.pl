c02e622d91b6bf36fab7f45070c6fed299b9c69f
Ian Campbell <ijc@hellion.org.uk>
2017-08-01 20:44:36 +0100

    Inline rewrite-paths.sed into flatten-symlinks.pl

--- scripts/flatten-symlinks.pl
+++ scripts/flatten-symlinks.pl
@@ -24,7 +24,49 @@ while (<$lsfiles>) {
 	    die "Failed to parse symlink $ENV{GIT_COMMIT}:$path $deref";
 	}
     }
-    print;
+
+    my $m = 0;
+
+    # Keep the copyright. Also ensures we never have a completely empty commit.
+    $m++ if m/\tCOPYING$/;
+
+    # A few architectures have dts files at non standard paths. Massage those into
+    # a standard arch/ARCH/boot/dts first.
+
+    # symlink: arch/microblaze/boot/dts/system.dts -> ../../platform/generic/system.dts
+    next if m,\tarch/microblaze/boot/dts/system.dts$,;
+    $m++ if s,\tarch/microblaze/platform/generic/(system.dts)$,\tarch/microblaze/boot/dts/$1,;
+
+    # arch/mips/lantiq/dts/easy50712.dts
+    # arch/mips/lantiq/dts/danube.dtsi
+    # arch/mips/netlogic/dts/xlp_evp.dts
+    # arch/mips/ralink/dts/rt3050.dtsi
+    # arch/mips/ralink/dts/rt3052_eval.dts
+    $m++ if s,\tarch/mips/([^/]*)/dts/(.*\.dts.?)$,\tarch/mips/boot/dts/$2,;
+
+    # arch/mips/cavium-octeon/octeon_68xx.dts
+    # arch/mips/cavium-octeon/octeon_3xxx.dts
+    # arch/mips/mti-sead3/sead3.dts
+    $m++ if s,\tarch/mips/([^/]*)/([^/]*\.dts.?)$,\tarch/mips/boot/dts/$2,;
+
+    # arch/x86/platform/ce4100/falconfalls.dts
+    $m++ if s,\tarch/x86/platform/ce4100/falconfalls.dts,\tarch/x86/boot/dts/falconfalls.dts,;
+
+    # test cases
+    $m++ if s,\tdrivers/of/testcase-data/,\ttestcase-data/,;
+
+    # Now rewrite generic DTS paths
+    $m++ if s,\tarch/([^/]*)/boot/dts/(.*\.dts.?)$,\tsrc/$1/$2,;
+    $m++ if s,\tarch/([^/]*)/boot/dts/(.*\.h)$,\tsrc/$1/$2,;
+
+    # Also rewrite the DTS include paths for dtc+cpp support
+    $m++ if s,\tarch/([^/]*)/include/dts/,\tsrc/$1/include/,;
+    $m++ if s,\tinclude/dt-bindings/,\tinclude/dt-bindings/,;
+
+    # Rewrite the bindings subdirectory
+    $m++ if s,\tDocumentation/devicetree/bindings/,\tBindings/,;
+
+    print if $m > 0;
 }
 kill $pid if $pid;
 exit 0;
--- scripts/index-filter.sh
+++ scripts/index-filter.sh
@@ -4,7 +4,6 @@ set -e
 set -o pipefail
 
 ${SCRIPTS}/flatten-symlinks.pl | \
-	sed -n -f ${SCRIPTS}/rewrite-paths.sed | \
 	GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
 
 if [ -f "$GIT_INDEX_FILE.new" ] ; then
