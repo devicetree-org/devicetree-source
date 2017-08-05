6b2bf6ce5c5cf64eafabe188b4aa045eeeb6028d
Ian Campbell <ijc@hellion.org.uk>
2017-08-05 10:46:54 +0000

    Add /usr/lib/git-core to path for git-filter-branch
    Previously this was in git-filter-branch because it was derived from the
    Debian git package version, but now it is based on the upstream source
    add it ourselves.

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -27,7 +27,7 @@ rm -f .git/refs/original/refs/heads/${UPSTREAM_REWRITTEN}
 
 git branch -f $UPSTREAM_REWRITTEN FETCH_HEAD
 
-sh $SCRIPTS/git-filter-branch --force \
+PATH=/usr/lib/git-core:$PATH sh $SCRIPTS/git-filter-branch --force \
 	--index-filter ${SCRIPTS}/index-filter.sh \
 	--msg-filter 'cat && /bin/echo -e "\n[ upstream commit: $GIT_COMMIT ]"' \
 	--tag-name-filter 'while read t ; do /bin/echo -n $t-dts-raw ; done' \
